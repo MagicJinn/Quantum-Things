eecf2aa57906597e85ba940534427d19661be7ef
MagicJinn <jinn.bz@mail.com>
2025-11-15 18:40:11 +0100

    removed unfinished sakenade

--- src/main/java/lumien/randomthings/worldgen/WorldGenPlants.java
+++ src/main/java/lumien/randomthings/worldgen/WorldGenPlants.java
@@ -22,7 +22,7 @@ public class WorldGenPlants extends WorldGenerator
 		if (world.getWorldType() != WorldType.DEBUG_ALL_BLOCK_STATES)
 		{
 			// Beans
-			if (Worldgen.beans && random.nextInt(Worldgen.BEANS_CHANCE) == 0)
+			if (Worldgen.BEANS && random.nextInt(Worldgen.BEANS_CHANCE) == 0)
 			{
 				int x = position.getX() + 8 + random.nextInt(16);
 				int z = position.getZ() + 8 + random.nextInt(16);
@@ -36,7 +36,7 @@ public class WorldGenPlants extends WorldGenerator
 			}
 
 			// Pitcher Plants
-			if (Worldgen.pitcherPlants && random.nextInt(Worldgen.PITCHER_PLANTS_CHANCE) == 0)
+			if (Worldgen.PITCHER_PLANTS && random.nextInt(Worldgen.PITCHER_PLANTS_CHANCE) == 0)
 			{
 				int x = position.getX() + 8 + random.nextInt(16);
 				int z = position.getZ() + 8 + random.nextInt(16);
