da633fe477b6041d39b9c3a34e7b6a47cec6b67c
MagicJinn <jinn.bz@mail.com>
2025-12-02 04:36:01 +0100

    unfucks workspace maybe

--- unfuck_workspace.bat
+++ unfuck_workspace.bat
@@ -1,3 +1,12 @@
+@echo off
+echo Cleaning workspace...
 gradlew clean
+echo Setting up decomp workspace...
 gradlew setupDecompWorkspace
+echo Generating Eclipse project files (this will also fix .project automatically)...
 gradlew eclipse
+
+echo.
+echo Done! Your workspace should now be properly configured.
+echo The build folder will be excluded from Eclipse indexing.
+pause
